c8d5ce7bb5f393023b46c69025190b99fc64cad4
draxemma <draxemma@kkg-do.eu>
2026-01-20 08:39:33 +0100

    20.01.2026

--- scr/Infrastruktur.lua
+++ scr/Infrastruktur.lua
@@ -25,51 +25,48 @@ function zeichneHaus(x,y,w,h)
     love.graphics.rectangle("fill",x,y,w,h)
 end
 
+function zeichneSeite1(x,y,w,h)
+    love.graphics.setColor(0, 0, 0)
+    love.graphics.rectangle("fill",x,y,w,h)
+end
+
+function zeichneDach()
+    love.graphics.setColor(0, 0, 0)
+    love.graphics.polygon("fill",200, 100, 400, 100, 300, 50 )
+end
+
 function zeichneFenster(x,y)
     love.graphics.setColor(235/255, 229/255, 52/255)
     love.graphics.rectangle("fill",x,y,40,40)
 end
 
 function zeichneHaus1 ()
-    zeichneHaus(0,150, 180,400)
-    zeichneFenster(10,180)
-    zeichneFenster(65,180)
-    zeichneFenster(120,180)
-    zeichneFenster(10,240)
-    zeichneFenster(65,240)
-    zeichneFenster(120,240)
-    zeichneFenster(10,300)
-    zeichneFenster(65,300)
-    zeichneFenster(120,300)
-    zeichneFenster(10,360)
-    zeichneFenster(65,360)
-    zeichneFenster(120,360)
+    zeichneHaus(0,200, 130,380)
+
+    zeichneFenster(10,220)
+    zeichneFenster(65,220)
+
+    zeichneFenster(10,270)
+    zeichneFenster(65,270)
+
+    zeichneFenster(10,320)
+    zeichneFenster(65,320)
+
+    zeichneFenster(10,370)
+    zeichneFenster(65,370)
+
     zeichneFenster(10,420)
     zeichneFenster(65,420)
-    zeichneFenster(120,420)
-    zeichneFenster(10,480)
-    zeichneFenster(65,480)
-    zeichneFenster(120,480)
+
+    zeichneFenster(10,470)
+    zeichneFenster(65,470)
+
 end
 
 function zeichneHaus2 ()
-    zeichneHaus(180,200, 120,  350)
- --   zeichneFenster(235,240)
-    zeichneFenster(190,240)
-    zeichneFenster(245,240)
---    zeichneFenster(235,300)
-    zeichneFenster(190,300)
-    zeichneFenster(245,300)
---    zeichneFenster(235,360)
-    zeichneFenster(190,360)
-    zeichneFenster(245,360)
- --   zeichneFenster(235,420)
-    zeichneFenster(190,420)
-    zeichneFenster(245,420)
-   -- zeichneFenster(235,480)
-    zeichneFenster(190,480)
-    zeichneFenster(245,480)
-end
+    zeichneHaus(200,100,200,400)
+    zeichneDach(200, 100,400, 100, 300, 50)
+    end
 
 function zeichneStreifen (xpos)
     love.graphics.setColor(1, 1, 1)
