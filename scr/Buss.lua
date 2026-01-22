89e6d28e9edd88bbaf132aac2bd118f003825ea2
Callandor-droid <draxhenn@kkg-do.eu>
2026-01-22 16:22:49 +0100

    changes 21.01.2026

--- scr/Buss.lua
+++ scr/Buss.lua
@@ -6,5 +6,5 @@ function zeichneBus ()
     love.graphics.setColor (345/255, 245/255, 50/250)
     love.graphics.rectangle ("fill", 150,400,200, 120)
     love.graphics.setColor (0, 0, 0)
-    love.graphics.circle ("fill", )
+    love.graphics.circle ("fill",100, 100, 50)
 end
