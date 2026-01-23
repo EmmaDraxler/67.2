872dcebdf71d114253461000d3d0b6db717f7bd3
draxemma <draxemma@kkg-do.eu>
2026-01-23 11:24:33 +0100

    23.02.2026 11:25

--- scr/Buss.lua
+++ scr/Buss.lua
@@ -4,17 +4,18 @@ function Buss.draw ()
 end
 function zeichneBus ()
     love.graphics.setColor (345/255, 245/255, 50/250)
-    love.graphics.rectangle ("fill", bus,busy,200, 120)
+    love.graphics.rectangle ("fill", bus*s ,busy,200*s, 120*s)
     love.graphics.setColor (0, 0, 0)
-    love.graphics.circle ("fill",rad1, rad1y, 20)
-    love.graphics.circle ("fill",rad2, rad2y, 20)
+    love.graphics.circle ("fill",rad1*s, rad1y, 20*s)
+    love.graphics.circle ("fill",rad2*s, rad2y, 20*s)
     love.graphics.setColor (116/255, 221/255, 242/250)
-    love.graphics.rectangle ("fill", f1,f1y,60, 40)
-    love.graphics.rectangle ("fill", f2,f2y,60, 40)
-    love.graphics.rectangle ("fill", f3,f3y,60, 40)
+    love.graphics.rectangle ("fill", f1*s,f1y,60*s, 40*s)
+    love.graphics.rectangle ("fill", f2*s,f2y,60 *s, 40*s)
+    love.graphics.rectangle ("fill", f3*s,f3y,60*s, 40*s)
 end
 
 function Buss.load ()
+    s = 1
     bus = 150
     rad1 = 180
     rad2 = 320
@@ -38,11 +39,12 @@ function Buss.update(dt)
     f3 = f3 + dt*80
 
     if zustand == 4 then
-        busy = busy - dt*80
-        rad1y = rad1y - dt*80
-        rad2y = rad2y - dt*80
-        f1y = f1y - dt*80
-        f2y = f2y - dt*80
-        f3y = f3y - dt*80
+        s = s - 0.1*dt
+        busy = busy - dt*40
+        rad1y = rad1y - dt*40
+        rad2y = rad2y - dt*40
+        f1y = f1y - dt*40
+        f2y = f2y - dt*40
+        f3y = f3y - dt*40
     end
 end
