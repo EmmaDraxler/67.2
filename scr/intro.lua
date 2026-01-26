2a90375b73f0ef398fa85ab9c76ccff4e7d13d86
medhu95 <malwmedh@kkg-do.eu>
2026-01-26 18:38:19 +0100

    Film von Emma, Raquel und Medhavi

--- scr/intro.lua
+++ scr/intro.lua
@@ -1,10 +1,12 @@
 intro = {}
 
 function intro.load()
+    myFont = love.graphics.newFont(50)
     countdownTimer = 4
 end
 
 function intro.draw()
+    love.graphics.setFont(myFont)
     love.graphics.print(math.floor(countdownTimer), 400, 300)
 
 end
